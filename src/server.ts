67276d25948f417c3b72e0539b11c4c6ae35b59a
Elvis020 <spaceyel3@gmail.com>
2022-08-03 15:21:41 +0000

    Added some logic but blocked, cant get to do a post request or figure out how to send a file

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import * as AWS from "./aws";
 
 (async () => {
 
   // Init the Express application
   const app = express();
+  const multer  = require('multer')
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -31,8 +33,71 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
   /**************************************************************************** */
+
+  // Function to send the file to the S3 bucket
+  async function sendFilesToBucket(file:string) {
+    const path = file.split("/")
+    const filename = path.pop()
+    const rmy_pop = path.join(",")
+    const extension = filename.split(".").pop()
+    // const upload = multer({ dest: `${path.join(",").replace(",","/")}/` })
+    try{
+      const send_item = AWS.getPutSignedUrl(filename)
+      const url_path =  AWS.getGetSignedUrl(send_item)
+      // a post request of the image with a valid signed-url
+      // app.post(url_path, upload.single(filename),async (req:Request,res:Response) => {
+      //   return res.status(200)
+      //       .contentType(`${extension == 'jpg'} ? image/jpeg : image/${extension}`)
+      //       .send(`Successfully uploaded image: ${path}/${filename}`)
+      // })
+      const lerrf = rmy_pop.replace(",","/")
+      console.log(`Filename: ${filename}, Path: ${lerrf}, Extension: ${extension}`);
+
+    }catch (err){
+      console.error(err)
+    }
+  }
+
+
+
+
+
+
+
+  
+  app.get("/filteredimage/",async(req:Request,res:Response) => {
+    let {imageUrl} = req.query
+    const arrayOfFiles = [] 
+    if (!imageUrl){
+      res.status(404).send(`ImageURL: ${imageUrl} cannot be found.`)
+    }
+    // This downloads the image and prepends it with a file path
+    const result = await filterImageFromURL(imageUrl)
+    arrayOfFiles.push(result)
+
+    // Send the files to the S3 bucket
+    // logic to upload the file
+    const path = result.split("/")
+    const filename = path.pop()
+    const send_item = AWS.getPutSignedUrl(filename)
+    const url_path =  AWS.getGetSignedUrl(send_item)
+    if(url_path){
+      console.log("I am here!");
+      console.log("Path:"+url_path);
+      app.post(url_path, (req:Request,res:Response) => {
+        res.sendFile(__dirname+"/util/tmp/"+filename, function (err:Error){
+          if(err){
+            res.status(400).send("Malformed File")
+          }
+          // await deleteLocalFiles(arrayOfFiles)
+          res.status(201).send("Image sent!")
+        });
+      })
+    }
+    await deleteLocalFiles(arrayOfFiles)
+    console.log("Done with process!");
+  });
 
   //! END @TODO1
   
